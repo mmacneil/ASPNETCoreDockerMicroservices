6a2d3b9af0825dc55da59d0e0141888af63d09d0
mark.macneil <mark.macneil@smith.co>
2017-12-18 11:19:50 -0400

    Bump wait time to run sql create script

--- Database/SqlCmdStartup.sh
+++ Database/SqlCmdStartup.sh
@@ -1,4 +1,4 @@
-#wait for the SQL Server to come up
-sleep 25
+#wait for the SQL Server to start up
+sleep 50
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Pass@word -d master -i SqlCmdScript.sql
