b6208f9626e34939146d2094daaf4208f1e0d049
mark.macneil <mark.macneil@smith.co>
2017-12-18 13:27:05 -0400

    Tweak delay on sql create script.

--- Database/SqlCmdStartup.sh
+++ Database/SqlCmdStartup.sh
@@ -1,4 +1,4 @@
 #wait for the SQL Server to start up
-sleep 50
+sleep 25
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Pass@word -d master -i SqlCmdScript.sql
